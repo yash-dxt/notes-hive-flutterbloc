c87de71773db66f612b1534183aa011900b7062e
Yash Dixit <52655208+udzhsay@users.noreply.github.com>
2020-09-25 02:27:35 +0530

    Warning and Refactoring

--- lib/screens/edit_note.dart
+++ lib/screens/edit_note.dart
@@ -3,98 +3,166 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_hive/bloc/note_bloc.dart';
 import 'package:notes_hive/models/note_model.dart';
 
-class EditNote extends StatelessWidget {
+class EditNote extends StatefulWidget {
   EditNote({this.note, this.index, @required this.newNote});
   final bool newNote;
   final Note note;
   final int index;
 
   @override
-  Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
+  _EditNoteState createState() => _EditNoteState();
+}
 
-    String title = note != null ? note.title : "";
-    String content = note != null ? note.content : "";
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text(
-          newNote ? 'New Note' : 'Edit Note',
-          style: TextStyle(color: Colors.white),
+class _EditNoteState extends State<EditNote> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String title = widget.note != null ? widget.note.title : "";
+    String content = widget.note != null ? widget.note.content : "";
+    final _formKey = GlobalKey<FormState>();
+    final _titleController = TextEditingController(text: title);
+    final _contentController = TextEditingController(text: content);
+
+    Future<bool> _onWillPop() async {
+      if (_titleController.text == title ||
+          _contentController.text == content) {
+        return showDialog(
+              context: context,
+              child: new AlertDialog(
+                title: new Text('Are you sure?'),
+                content: new Text('Unsaved data will be lost.'),
+                actions: <Widget>[
+                  new FlatButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: new Text('No'),
+                  ),
+                  new FlatButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: new Text('Yes'),
+                  ),
+                ],
+              ),
+            ) ??
+            false;
+      } else {
+        return true;
+      }
+    }
+
+    return WillPopScope(
+      onWillPop: () => _onWillPop(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text(
+            widget.newNote ? 'New Note' : 'Edit Note',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Color(0xFF5b696f),
         ),
-        backgroundColor: Color(0xFF5b696f),
-      ),
-      floatingActionButton: Padding(
-          padding: EdgeInsets.only(left: 10, bottom: 10),
-          child: FloatingActionButton.extended(
-            icon: Icon(Icons.add),
-            label: Text(newNote ? 'ADD' : 'UPDATE'),
-            onPressed: () {
-              if (_formKey.currentState.validate()) {
-                newNote
-                    ? BlocProvider.of<NoteBloc>(context)
-                        .add(NoteAddEvent(title: title, content: content))
-                    : BlocProvider.of<NoteBloc>(context).add(NoteEditEvent(
-                        title: title, content: content, index: index));
-                Navigator.pop(context);
-              }
-            },
-          )),
-      body: SingleChildScrollView(
-        child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  padding: EdgeInsets.only(left: 10, right: 10),
-                  child: TextFormField(
-                    initialValue: newNote ? '' : note.title,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter the note title';
-                      }
-                      return null;
-                    },
-                    onChanged: (text) {
-                      title = text;
-                    },
-                    decoration: InputDecoration(
-                      labelStyle: TextStyle(color: Colors.black38),
-                      border: OutlineInputBorder(),
-                      labelText: 'Note Title',
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  padding: EdgeInsets.only(left: 10, right: 10),
-                  child: TextFormField(
-                    initialValue: newNote ? '' : note.content,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter the note body!';
-                      }
-                      return null;
-                    },
-                    onChanged: (text) {
-                      content = text;
-                    },
-                    maxLines: 90,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                )
-              ],
+        floatingActionButton: Padding(
+            padding: EdgeInsets.only(left: 10, bottom: 10),
+            child: FloatingActionButton.extended(
+              icon: Icon(Icons.add),
+              label: Text(widget.newNote ? 'ADD' : 'UPDATE'),
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  widget.newNote
+                      ? BlocProvider.of<NoteBloc>(context).add(NoteAddEvent(
+                          title: _titleController.text,
+                          content: _contentController.text))
+                      : BlocProvider.of<NoteBloc>(context).add(NoteEditEvent(
+                          title: _titleController.text,
+                          content: _contentController.text,
+                          index: widget.index));
+                  Navigator.pop(context);
+                }
+              },
             )),
+        body: SingleChildScrollView(
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  NoteTitle(titleController: _titleController),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  NoteBody(contentController: _contentController),
+                  SizedBox(
+                    height: 10,
+                  )
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
+
+class NoteTitle extends StatelessWidget {
+  const NoteTitle({
+    Key key,
+    @required TextEditingController titleController,
+  })  : _titleController = titleController,
+        super(key: key);
+
+  final TextEditingController _titleController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: TextFormField(
+        controller: _titleController,
+        validator: (value) {
+          if (value.isEmpty) {
+            return 'Please enter the note title';
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+          labelStyle: TextStyle(color: Colors.black38),
+          border: OutlineInputBorder(),
+          labelText: 'Note Title',
+        ),
+      ),
+    );
+  }
+}
+
+class NoteBody extends StatelessWidget {
+  const NoteBody({
+    Key key,
+    @required TextEditingController contentController,
+  })  : _contentController = contentController,
+        super(key: key);
+
+  final TextEditingController _contentController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: TextFormField(
+        controller: _contentController,
+        validator: (value) {
+          if (value.isEmpty) {
+            return 'Please enter the note body!';
+          }
+          return null;
+        },
+        maxLines: 90,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+        ),
       ),
     );
   }
